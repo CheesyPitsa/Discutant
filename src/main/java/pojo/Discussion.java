b7c99f873eedd9c005315446c6184ab8ce2cc386
Antonova Aleksandra <alex.andra200202@bk.ru>
2024-05-02 19:06:54 +0500

    Работа над дискуссиями

--- src/main/java/pojo/Discussion.java
+++ src/main/java/pojo/Discussion.java
@@ -1,7 +1,31 @@
 package pojo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-public class Discussion {
+import java.util.Date;
+import java.util.Set;
 
-
+@NoArgsConstructor @Getter @Setter
+@Document (collection = "discussion")
+public class Discussion
+{
+    @Id
+    private ObjectId id;
+    private String name;
+    @DBRef
+    private User creator;
+    private Date date;
+    @DBRef
+    private Chat chat;
+    private boolean nullified;
+    private Set<User> views;
+    private String steno;
+    private String video;
 }
+
